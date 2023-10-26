9994cc1b6251178c1e7ddc8be2fc400eb2385e86
Azm1l <azmilawaludin@gmail.com>
2023-10-26 15:10:12 +0700

    remark primary

--- app/ModelConsignee.php
+++ app/ModelConsignee.php
@@ -9,7 +9,7 @@ class ModelConsignee extends Model
     public $timestamps = true;
     protected $table = 'consignee';
 
-    protected $primaryKey = 'consignee_id';
+    //protected $primaryKey = 'consignee_id';
     protected $fillable = ['consignee_name'];
     const CREATED_AT = 'create_datetime';
     const UPDATED_AT = 'update_datetime';
