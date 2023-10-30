5a56bcc5537b71ff6e9a31bc447d1a2dc48c7ffc
Azm1l <azmilawaludin@gmail.com>
2023-10-30 13:23:37 +0700

    remark

--- routes/web.php
+++ routes/web.php
@@ -45,10 +45,12 @@ $app->group(['prefix' => 'masterlocation'], function () use ($app) {
     $app->post('/', 'MasterLocationController@addData');
 });
 
+/*
 $app->group(['prefix' => 'checkin'], function () use ($app) {;
     $app->get('/{id}', 'CheckInController@singleData');
     $app->post('/', 'CheckInController@addData');
 });
+*/
 
 $app->post('/updatebulk', 'UpdateBulkController@addData');
 
