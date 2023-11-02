bfd8618b2d1a12bcb13572a8d9fd80c51de711cd
Azm1l <azmilawaludin@gmail.com>
2023-11-02 13:31:19 +0700

    delete migration

--- database/migrations/2023_11_02_063054_checkin.php
+++ database/migrations/2023_11_02_063054_checkin.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class TableTodo extends Migration
+class Checkin extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class TableTodo extends Migration
      */
     public function up()
     {
-        Schema::create('todo', function (Blueprint $table) {
+        Schema::create('checkin', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('activity',200);
-            $table->text('description');
             $table->timestamps();
         });
     }
@@ -28,8 +26,6 @@ class TableTodo extends Migration
      */
     public function down()
     {
-        Schema::create('todo', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('checkin');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -45,12 +45,12 @@ $app->group(['prefix' => 'masterlocation'], function () use ($app) {
     $app->post('/', 'MasterLocationController@addData');
 });
 
-/*
+
 $app->group(['prefix' => 'checkin'], function () use ($app) {;
     $app->get('/{id}', 'CheckInController@singleData');
     $app->post('/', 'CheckInController@addData');
 });
-*/
+
 
 $app->post('/updatebulk', 'UpdateBulkController@addData');
 $app->get('/masterstatus', 'MasterStatusController@index');
