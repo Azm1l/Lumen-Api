c400cd12cfa14dd836baea64845acfbade02faf2
Azm1l <azmilawaludin@gmail.com>
2023-11-02 15:06:20 +0700

    add device id

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -31,7 +31,8 @@ class AuthController extends Controller
     {
         $this->validate($this->request, [
             'username'     => 'required|string',
-            'password'  => 'required|string'
+            'password'  => 'required|string',
+            'deviceId' => 'required|string',
         ]);
 
         $user = ModelUsers::where('username', $this->request->input('username'))->first();
@@ -41,6 +42,17 @@ class AuthController extends Controller
                 'message' => 'Username does not exist.'
             ], 400);
         }
+
+        if ($user->device_id == null) {
+            $user->device_id = $this->request->input('deviceId');
+            $user->save();
+        } else if ($user->device_id != $this->request->input('deviceId')) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Device ID does not match.'
+            ], 400);
+        }
+
         if ($this->request->input('password') == $user->password) {
             return response()->json([
                 'success' => true,
@@ -48,6 +60,7 @@ class AuthController extends Controller
                 'user' => $user
             ], 200);
         }
+
         return response()->json([
             'success' => false,
             'message' => 'Username or password is wrong.'
